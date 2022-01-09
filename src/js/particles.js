d3332e670bbe30973081204ebf94063553d35bd5
Ramses Martinez <ramses.mtz96@gmail.com>
2022-01-09 15:43:46 -0600

    cambiuo estilos

--- src/js/particles.js
+++ src/js/particles.js
@@ -2,7 +2,30 @@ window.onload = function() {
     console.log('init')
     Particles.init({
         selector: '.background',
+        maxParticles: 100,
         connectParticles: true,
-        color: '#c7d0e2'
+        color: '#c7d0e2',
+        responsive: [
+            {
+                breakpoint: 768,
+                options: {
+                    maxParticles: 90,
+                    color:  '#48F2E3',
+                    connectParticles: false
+                }
+            }, {
+                breakpoint: 425,
+                options: {
+                    maxParticles: 80,
+                    connectParticles: false
+                }
+            }, {
+                breakpoint: 320,
+                options: {
+                    maxParticles: 70,
+                    connectParticles: false
+                }
+            }
+        ]
     });
 };
